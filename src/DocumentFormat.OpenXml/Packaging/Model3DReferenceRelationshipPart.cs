c44fe1c7f125524247e6c4f32ea2b13f1ca11ca0
oKyokoArita <135585319+oKyokoArita@users.noreply.github.com>
2024-01-20 02:07:47 +0900

    update file

---
+++ src/DocumentFormat.OpenXml/Packaging/Model3DReferenceRelationshipPart.cs
@@ -0,0 +1,23 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace DocumentFormat.OpenXml.Packaging
+{
+    /// <summary>
+    /// Defines the Model3DReferenceRelationshipPart
+    /// </summary>
+    public partial class Model3DReferenceRelationshipPart
+    {
+        /// <inheritdoc/>
+        private protected override bool IsValidContentType(IPackagePart part)
+        {
+            if (base.IsValidContentType(part))
+            {
+                return true;
+            }
+
+            // model/gltf.binary is an acceptable MIME for this part, PowerPoint writes this on save.
+            return part.ContentType == "model/gltf.binary";
+        }
+    }
+}
