951e20af0483623e49fd1c409637eb5b73773bdc
oKyokoArita <135585319+oKyokoArita@users.noreply.github.com>
2024-01-20 02:12:41 +0900

    update file

---
+++ src/DocumentFormat.OpenXml/Schema/Spreadsheet/CellType.cs
@@ -0,0 +1,40 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using DocumentFormat.OpenXml.Framework;
+using DocumentFormat.OpenXml.Validation;
+using System.Globalization;
+
+namespace DocumentFormat.OpenXml.Spreadsheet
+{
+    public partial class CellType : IValidator
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1863:Use 'CompositeFormat'", Justification = "Optimization not needed")]
+        void IValidator.Validate(ValidationContext context)
+        {
+            if (DataType is null || !DataType.HasValue)
+            {
+                return;
+            }
+
+            if (CellValue is CellValue value)
+            {
+                var success = DataType.Value switch
+                {
+                    var b when b == CellValues.Boolean => value.TryGetBoolean(out _),
+                    var d when d == CellValues.Date => value.TryGetDateTimeOffset(out _) || value.TryGetDateTime(out _),
+                    var n when n == CellValues.Number => value.TryGetInt(out _) || value.TryGetDouble(out _) || value.TryGetDecimal(out _),
+                    _ => true,
+                };
+
+                if (!success)
+                {
+                    context.CreateError(
+                        id: "Sem_CellValue",
+                        errorType: ValidationErrorType.Semantic,
+                        description: string.Format(CultureInfo.CurrentCulture, ValidationResources.Sem_CellValue, value.InnerText, DataType.Value));
+                }
+            }
+        }
+    }
+}
