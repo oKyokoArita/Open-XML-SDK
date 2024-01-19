addf9aab1f4ecee021cd3d9e5ca489dba49d0d8c
oKyokoArita <135585319+oKyokoArita@users.noreply.github.com>
2024-01-20 02:06:34 +0900

    update file

---
+++ src/DocumentFormat.OpenXml.Framework/ExperimentalApis.cs
@@ -0,0 +1,15 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace DocumentFormat.OpenXml;
+
+internal static class ExperimentalApis
+{
+    internal const string Message = "This is an experimental APIs and is subject to change";
+    internal const string UrlFormat = "https://aka.ms/openxmlsdk-diagnostics#{0}";
+
+    public const string IPackage = "OOXML0001";
+    public const string PackageBuilder = "OOXML0002";
+    public const string AOT = "OOXML0003";
+    public const string Framework = "OOXML0004";
+}
