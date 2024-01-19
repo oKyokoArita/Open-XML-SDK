6d16d87ca693128d396c9626967d11f9a317bea4
oKyokoArita <135585319+oKyokoArita@users.noreply.github.com>
2024-01-20 02:07:57 +0900

    update file

---
+++ src/DocumentFormat.OpenXml.Features/RandomNumberGenerator/RandomNumberGeneratorFeatureExtensions.cs
@@ -0,0 +1,51 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using DocumentFormat.OpenXml.Packaging;
+using System;
+using System.Security.Cryptography;
+
+namespace DocumentFormat.OpenXml.Features
+{
+    /// <summary>
+    /// Extensions to add a random number generator to a feature set.
+    /// </summary>
+    public static class RandomNumberGeneratorFeatureExtensions
+    {
+        /// <summary>
+        /// Add a random number generator to the package.
+        /// </summary>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Reliability", "CA2000:Dispose objects before losing scope", Justification = "Registered for disposable on package")]
+        public static void AddRandomNumberGeneratorFeature(this OpenXmlPackage package)
+        {
+            if (package is null)
+            {
+                throw new ArgumentNullException(nameof(package));
+            }
+
+            if (package.Features.Get<IRandomNumberGeneratorFeature>() is null)
+            {
+                var feature = new RandomNumberGeneratorFeature();
+                package.Features.GetRequired<IDisposableFeature>().Register(feature);
+                package.Features.Set<IRandomNumberGeneratorFeature>(feature);
+            }
+        }
+
+        private sealed class RandomNumberGeneratorFeature : IRandomNumberGeneratorFeature, IDisposable
+        {
+            private readonly RandomNumberGenerator _generator = RandomNumberGenerator.Create();
+
+            public void Dispose()
+            {
+#if !NET35
+                _generator.Dispose();
+#endif
+            }
+
+            public void GetBytes(byte[] bytes)
+            {
+                _generator.GetBytes(bytes);
+            }
+        }
+    }
+}
