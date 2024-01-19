eaf74e05e6bbc9a2632a02a81547771718bce6d4
oKyokoArita <135585319+oKyokoArita@users.noreply.github.com>
2024-01-20 02:07:17 +0900

    update file

---
+++ gen/DocumentFormat.OpenXml.Generator/NamespaceGeneration/NamespaceGenerator.cs
@@ -0,0 +1,30 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using DocumentFormat.OpenXml.Generator.Models;
+using Microsoft.CodeAnalysis;
+using System.Collections.Immutable;
+
+namespace DocumentFormat.OpenXml.Generator.NamespaceGeneration;
+
+public static class NamespaceGenerator
+{
+    private static readonly DiagnosticDescriptor MalformedDataFileDescriptor = new("OOX3000", "Malformed data file", "Failed to load data file", "Data", DiagnosticSeverity.Error, isEnabledByDefault: true);
+
+    public static void WriteNamespaces(SourceProductionContext context, ImmutableArray<NamespaceInfo> namespaces)
+    {
+        if (namespaces.IsDefaultOrEmpty)
+        {
+            return;
+        }
+
+        try
+        {
+            context.AddSource("Namespaces.g.cs", namespaces.Generate());
+        }
+        catch (Exception)
+        {
+            context.ReportDiagnostic(Diagnostic.Create(MalformedDataFileDescriptor, location: null));
+        }
+    }
+}
