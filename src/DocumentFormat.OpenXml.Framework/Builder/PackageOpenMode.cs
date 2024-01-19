0dbd3089c063277f3e782cf9ae827639d0210200
oKyokoArita <135585319+oKyokoArita@users.noreply.github.com>
2024-01-20 02:08:00 +0900

    update file

---
+++ src/DocumentFormat.OpenXml.Framework/Builder/PackageOpenMode.cs
@@ -0,0 +1,30 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace DocumentFormat.OpenXml.Builder;
+
+/// <summary>
+/// An enum that describes how a package is going to be opened
+/// </summary>
+[Experimental(ExperimentalApis.PackageBuilder, UrlFormat = ExperimentalApis.UrlFormat)]
+[Obsolete(ExperimentalApis.Message, DiagnosticId = ExperimentalApis.PackageBuilder, UrlFormat = ExperimentalApis.UrlFormat)]
+internal enum PackageOpenMode
+{
+    /// <summary>
+    /// Indicates that a new package will be created.
+    /// </summary>
+    Create,
+
+    /// <summary>
+    /// Indicates that a package will be opened in read mode.
+    /// </summary>
+    Read,
+
+    /// <summary>
+    /// Indicates that a package will be opened in read/write mode.
+    /// </summary>
+    ReadWrite,
+}
