4946c368ffab9f8996ec58dfa83cf0bf2a31abb7
oKyokoArita <135585319+oKyokoArita@users.noreply.github.com>
2024-01-20 02:07:43 +0900

    update file

---
+++ src/DocumentFormat.OpenXml/Packaging/CustomPropertyPartType.cs
@@ -0,0 +1,21 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace DocumentFormat.OpenXml.Packaging
+{
+    /// <summary>
+    /// Defines CustomPropertyPartType - types of CustomPropertyPart.
+    /// </summary>
+    public static class CustomPropertyPartType
+    {
+        /// <summary>
+        /// Defines type information for spreadsheet custom property part.
+        /// </summary>
+        public static readonly PartTypeInfo Spreadsheet = new("application/vnd.openxmlformats-officedocument.spreadsheetml.customProperty", ".xml");
+
+        /// <summary>
+        /// Defines type information for xml custom property part.
+        /// </summary>
+        public static readonly PartTypeInfo Xml = new("application/xml", ".xml");
+    }
+}
