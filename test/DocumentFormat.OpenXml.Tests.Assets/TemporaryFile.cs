3e1f61de7591002c12e972534a8679373c99f108
oKyokoArita <135585319+oKyokoArita@users.noreply.github.com>
2024-01-20 02:07:15 +0900

    update file

---
+++ test/DocumentFormat.OpenXml.Tests.Assets/TemporaryFile.cs
@@ -0,0 +1,33 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.IO;
+
+namespace DocumentFormat.OpenXml.Tests
+{
+    public sealed class TemporaryFile : IFile
+    {
+        private TemporaryFile(string path)
+        {
+            Path = path;
+        }
+
+        public static IFile Create() => new TemporaryFile(System.IO.Path.GetTempFileName());
+
+        public string Path { get; }
+
+        public bool IsEditable => true;
+
+        public FileAccess Access => FileAccess.ReadWrite;
+
+        public Stream Open() => File.Open(Path, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+
+        public void Dispose()
+        {
+            if (File.Exists(Path))
+            {
+                File.Delete(Path);
+            }
+        }
+    }
+}
