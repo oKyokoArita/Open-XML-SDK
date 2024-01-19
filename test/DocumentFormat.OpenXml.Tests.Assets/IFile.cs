1ab2c4fdf774a82160f0e0615c7a00f35281821d
oKyokoArita <135585319+oKyokoArita@users.noreply.github.com>
2024-01-20 02:07:15 +0900

    update file

---
+++ test/DocumentFormat.OpenXml.Tests.Assets/IFile.cs
@@ -0,0 +1,19 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.IO;
+
+namespace DocumentFormat.OpenXml.Tests
+{
+    public interface IFile : IDisposable
+    {
+        string Path { get; }
+
+        Stream Open();
+
+        bool IsEditable { get; }
+
+        FileAccess Access { get; }
+    }
+}
