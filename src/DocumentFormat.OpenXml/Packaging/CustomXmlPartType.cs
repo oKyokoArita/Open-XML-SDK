2526ef164de79bf1b6ecc7c813da25676242de9b
oKyokoArita <135585319+oKyokoArita@users.noreply.github.com>
2024-01-20 02:07:43 +0900

    update file

---
+++ src/DocumentFormat.OpenXml/Packaging/CustomXmlPartType.cs
@@ -0,0 +1,31 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace DocumentFormat.OpenXml.Packaging
+{
+    /// <summary>
+    /// Defines CustomXmlPartType - types of CustomXmlPart.
+    /// </summary>
+    public static class CustomXmlPartType
+    {
+        /// <summary>
+        /// Defines type information for AdditionalCharacteristicsInfo part.
+        /// </summary>
+        public static readonly PartTypeInfo AdditionalCharacteristicsInfo = new("application/xml", ".xml");
+
+        /// <summary>
+        /// Defines type information for Bibliography part.
+        /// </summary>
+        public static readonly PartTypeInfo Bibliography = new("application/xml", ".xml");
+
+        /// <summary>
+        /// Defines type information for CustomXml part.
+        /// </summary>
+        public static readonly PartTypeInfo CustomXml = new("application/xml", ".xml");
+
+        /// <summary>
+        /// Defines type information for InkContent part.
+        /// </summary>
+        public static readonly PartTypeInfo InkContent = new("application/inkml+xml", ".xml");
+    }
+}
