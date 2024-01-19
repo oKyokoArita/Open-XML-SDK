134ef5f5ce2e2b50f1c0b28df03d8de822dc0bfe
oKyokoArita <135585319+oKyokoArita@users.noreply.github.com>
2024-01-20 02:08:12 +0900

    update file

---
+++ src/DocumentFormat.OpenXml.Framework/Features/IPackageInitializer.cs
@@ -0,0 +1,22 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using DocumentFormat.OpenXml.Packaging;
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace DocumentFormat.OpenXml.Features;
+
+/// <summary>
+/// An initializer for a package.
+/// </summary>
+[Experimental(ExperimentalApis.PackageBuilder, UrlFormat = ExperimentalApis.UrlFormat)]
+[Obsolete(ExperimentalApis.Message, DiagnosticId = ExperimentalApis.PackageBuilder, UrlFormat = ExperimentalApis.UrlFormat)]
+internal interface IPackageInitializer
+{
+    /// <summary>
+    /// Initializes a package.
+    /// </summary>
+    /// <param name="package">Package to initialize.</param>
+    void Initialize(OpenXmlPackage package);
+}
